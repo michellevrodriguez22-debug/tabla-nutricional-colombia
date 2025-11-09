5b6aa2e1b64b7c21612048a5f798722423fb281d
michellevrodriguez22-debug <michellev.rodriguez.22@gmail.com>
2025-11-08 20:30:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -373,6 +373,13 @@ def column_labels():
 
 # ============== Helper: medición y columnas compactas por contenido ==============
 def measure_text(draw, text, font):
+
+def draw_hline(draw, x0, x1, y, color, width):
+    draw.line((x0, y, x1, y), fill=color, width=width)
+
+def draw_vline(draw, x, y0, y1, color, width):
+    draw.line((x, y0, x, y1), fill=color, width=width)
+
     bbox = draw.textbbox((0,0), text, font=font)
     return bbox[2]-bbox[0], bbox[3]-bbox[1]
 
